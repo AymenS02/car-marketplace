ccaa4a07d84c95333c40e38aed053ec49543c961
Aymen Shoteri <aymenshoteri@gmail.com>
2024-09-19 23:00:57 -0400

    1 - project auth routes and controllers (47:00)

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,69 @@
+import { generateTokenAndSetCookie } from '../lib/utils/generateToken.js';
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+
 export const signup = async (req, res) => {
-  res.json({
-    data: 'You hit the signup endpoint!'
-  });
+  try {
+    const {fullName, username, email, password} = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        error: 'Email is not in a valid format'
+      });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({
+        error: 'Username is already taken'
+      });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({
+        error: 'Email is already taken'
+      });
+    }
+
+    // hash password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+  
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password: hashedPassword
+    });
+
+    if(newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      return res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        email: newUser.email,
+        profileImg: newUser.profileImg,
+        bio: newUser.bio,
+        link: newUser.link
+      });
+    } else {
+      return res.status(400).json({
+        error: 'Error saving user in database'
+      });
+    }
+    
+  } catch (error) {
+    console.error('Error on signup', error);
+    return res.status(500).json({
+      error: 'Internal Server Error'
+    });
+  }
 }
 
 export const login = async (req, res) => {
---
+++ backend/models/user.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 3,
+    },
+    
+    fullname: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,  
+    },
+
+    profileImg: {
+      type: String,
+      default: "",
+    },
+
+    bio: {
+      type: String,
+      default: "",
+    },
+
+    link: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
