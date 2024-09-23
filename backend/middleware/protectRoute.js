fc6c84a6142ede5746f9c3a62233a608b7749a1d
Aymen Shoteri <aymenshoteri@gmail.com>
2024-09-22 22:08:40 -0400

    restarting server

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
@@ -12,7 +13,7 @@ export const protectRoute = async (req, res, next) => {
 
 		if (!decoded) {
 			return res.status(401).json({ error: "Unauthorized: Invalid Token" });
-		}
+		}  
 
 		const user = await User.findById(decoded.userId).select("-password");
 
